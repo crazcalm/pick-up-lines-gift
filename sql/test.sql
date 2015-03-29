c7e7acb2253e032517d1014da60b5d4eb355b28a
Crazcalm <crazcalm@gmail.com>
2015-03-29 11:58:12 -0400

    Learning about sqlite3

---
+++ sql/test.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS pickuplines;
+
+CREATE TABLE pickuplines (
+    line    VARCHAR(250)        NOT NULL
+);
+
+DROP TABLE IF EXISTS comebacks;
+
+CREATE TABLE comebacks (
+    line    VARCHAR(250)        NOT NULL
+);
